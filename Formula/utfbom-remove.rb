1bd63be9eafa54dbb2f70e0ab2a22bec794d49bc
alastairruhm <alastairruhm@gmail.com>
2017-10-29 14:04:07 +0800

    utfbom-remove version v1.0.2

--- Formula/utfbom-remove.rb
+++ Formula/utfbom-remove.rb
@@ -1,12 +1,12 @@
 class UtfbomRemove < Formula
   desc "remove utf8 bom header of file"
   homepage "https://github.com/alastairruhm/utfbom-remove"
-  url "https://github.com/alastairruhm/utfbom-remove/releases/download/v1.0.1/utfbom-remove_1.0.1_macOS_64-bit.tar.gz"
-  version "1.0.1"
-  sha256 "1c830f3bbfb89d26fa9bfea35859824d822cea1c0bab604941962216f8363530"
+  url "https://github.com/alastairruhm/utfbom-remove/releases/download/v1.0.2/utfbom-remove_1.0.2_macOS_64-bit.tar.gz"
+  version "1.0.2"
+  sha256 "f5fa1f02c6cb017e2580fd6c3124bbfc693606a49a75a81faae4ed536bc24070"
 
   def install
-    bin.install "program"
+    bin.install "utfbom-remove"
   end
 
   test do
