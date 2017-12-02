450fccb01b168ff5f7c9b646d8879529905c4f6c
alastairruhm <alastairruhm@gmail.com>
2017-12-02 16:24:28 +0800

    zj-db-cluster version v0.3.1

--- Formula/zj-db-cluster.rb
+++ Formula/zj-db-cluster.rb
@@ -1,9 +1,9 @@
 class ZjDbCluster < Formula
   desc "zijin database cluster command line tool"
   homepage "https://github.com/alastairruhm/zj-db-cluster"
-  url "https://github.com/alastairruhm/zj-db-cluster/releases/download/v0.3.0/zj-db-cluster_0.3.0_macOS_64-bit.tar.gz"
-  version "0.3.0"
-  sha256 "0360183871cc96ef798cc65bbc5cbaf1f77486ce7b5ad483e11796baf224db91"
+  url "https://github.com/alastairruhm/zj-db-cluster/releases/download/v0.3.1/zj-db-cluster_0.3.1_macOS_64-bit.tar.gz"
+  version "0.3.1"
+  sha256 "63a80a02a348ec2914de440d8b8b2a87f0b508d2944ea11e8ab61cd9ddb59bd5"
 
   def install
     bin.install "zj-db-cluster"
