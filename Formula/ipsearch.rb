ef05ad62002265e2cf164165cf9477cfc3f0b1c7
alastairruhm <alastairruhm@gmail.com>
2018-06-01 16:46:21 +0800

    Brew formula update for ipsearch version v1.1.0

--- Formula/ipsearch.rb
+++ Formula/ipsearch.rb
@@ -1,9 +1,9 @@
 class Ipsearch < Formula
   desc "search ip location"
   homepage "https://github.com/alastairruhm/ipsearch"
-  url "https://github.com/alastairruhm/ipsearch/releases/download/v1.0.1/ipsearch_1.0.1_macOS_64-bit.tar.gz"
-  version "1.0.1"
-  sha256 "38c2fdf00c0a036450ed0218a438d1c11fa03f7631fa1c5dfbb988346edf08bc"
+  url "https://github.com/alastairruhm/ipsearch/releases/download/v1.1.0/ipsearch_1.1.0_macOS_64-bit.tar.gz"
+  version "1.1.0"
+  sha256 "b75428d46c2c45293fc805aaa5b7fc65342ae03d0579a74a556986c4ace7bc0f"
 
   def install
     bin.install "ipsearch"
